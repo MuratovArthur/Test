fe62918493e4daff7f47d2af32cfacdcabc9143c
MuratovArthur <arturlove45@gmail.com>
2020-08-30 23:05:59 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
 
+app.use(mongoose.connect('mongodb+srv://Elman:123456789n@test.deg0b.mongodb.net/Test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }));
+
+
 app.get("/", function (req, res){
    res.render('landing.ejs');
 });
